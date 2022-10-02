4145723a287f4202ab5065f54ec8ab5ddfd03f74
tomoyasuzuki <tomoya.s0121@icloud.com>
2022-10-02 21:55:44 +0900

    fix testdata

--- testdata/testdata.ts
+++ testdata/testdata.ts
@@ -9,7 +9,10 @@ const setupTestData = (num: number) => {
     email.senderName = "Test Account !!!!!!!!!てすとてすと";
     email.senderAddr = "hugahuga@hoge.com !!!!!!!!!!!!!!!!!!!!!!!!";
     email.body =
-      "hoge様\r\n\r\n役員会の資料をお送りいたします。\r\n以下のリンクから資料に アクセスできます。\r\n https://example.com \r\n\r\n よろしくお願いいたします。https://example.com  もし閲覧出来ない場合は https://hogehuga.com からお願いします。 \r\n\r\nhuga\r\n";
+      "hoge様\\r\\n\\r\\n役員会の資料をお送りいたします。\\r\\n以下のリンクから資料に\
+      アクセスできます。\\r\\n\\r\\nリンク : https://example.com\\r\\n\\r\\nよろしくお願いいたします。\\r\\n\\r\\n\
+      上記のリンクが閲覧出来ない場合は https://hogehuga.com からお願いします。\\r\\n\\r\\n\
+      お時間あれば、https://example.com も見て頂けますと幸いです。\\r\\n";
     email.receptionTime = "22:00";
 
     if (i % 2 == 0) {
