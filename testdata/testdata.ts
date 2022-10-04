2f0b4cbeeef4add12883e9ec334ab6905938bd8a
tomoyasuzuki <tomoya.s0121@icloud.com>
2022-10-04 22:00:11 +0900

    fix

--- testdata/testdata.ts
+++ testdata/testdata.ts
@@ -4,20 +4,20 @@ const setupTestData = (num: number) => {
   const testData: Email[] = [];
 
   for (let i = 0; i < num; i++) {
-    let email = new Email();
-    email.subject = "Hello👋 This is just a test email !!!!!!!!!!!!!!";
-    email.senderName = "Test Account !!!!!!!!!てすとてすと";
-    email.senderAddr = "hugahuga@hoge.com !!!!!!!!!!!!!!!!!!!!!!!!";
-    email.body =
+    const testBody =
       "hoge様\\r\\n\\r\\n役員会の資料をお送りいたします。\\r\\n以下のリンクから資料に\
       アクセスできます。\\r\\n\\r\\nリンク : https://example.com\\r\\n\\r\\nよろしくお願いいたします。\\r\\n\\r\\n\
       上記のリンクが閲覧出来ない場合は https://hogehuga.com からお願いします。\\r\\n\\r\\n\
       お時間あれば、https://example.com も見て頂けますと幸いです。\\r\\n";
-    email.receptionTime = "22:00";
 
-    if (i % 2 == 0) {
-      email.read = true;
-    }
+    let email = new Email(
+      "id",
+      "Test Account <test@test.com>",
+      "Hello👋 This is just a test email !!!!!!!!!!!!!!",
+      "22:00",
+      testBody,
+      ""
+    );
     testData.push(email);
   }
 
